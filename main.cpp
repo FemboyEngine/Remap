4d1d8413aba1cf797779688eb4b2249914ada8f3
MoleSchizo <molel768@gmail.com>
2023-10-13 20:55:27 +0300

    docs: add LICENSE

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 /*
-* Remap
-* A Memory alteration utility.
-*/
+ *  Remap - A Memory Alteration Utility
+ *
+ *  Remap is an open-source memory alteration utility designed to provide users
+ *  with powerful tools for interacting with memory and processes. This software
+ *  is distributed under the GNU General Public License (GPL) and is free to
+ *  use, modify, and distribute under the terms and conditions of the GPL.
+ *  Please refer to the 'LICENSE' file for the full text of the GNU GPL License.
+ *
+ *  File: main.cpp
+ *  Description: The entry point of the Remap utility.
+ *  This file initializes the renderer, creates the main window, and handles
+ *  the main loop.
+ *
+ *  GitHub: https://github.com/FemboyEngine/remap
+ */
 
 #include "interface/renderer.h"
 
